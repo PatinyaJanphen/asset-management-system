ea1fb76871cc9d99703a8af74ab0916626ad4ab3
Fifa <108819737+PatinyaJanphen@users.noreply.github.com>
2025-10-21 23:40:51 +0700

    bigiint

--- Backend/controllers/reportController.js
+++ Backend/controllers/reportController.js
@@ -218,7 +218,7 @@ export const exportAnnualByUserReportCSV = async (req, res) => {
                 lt: new Date(year + 1, 0, 1)
             },
             ownerId: {
-                in: userIds.map(id => BigInt(id))
+                in: userIds.map(user => BigInt(user.id))
             }
         };
 
